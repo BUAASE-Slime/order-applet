b905a5465d960ba9644e1e08d4494f0a4a02ead5
zewanhuang <zewanhuang@126.com>
2021-06-09 02:29:20 +0800

    feat: handle exception

---
+++ springboot/src/main/java/com/example/demo/handle/DianCanExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.demo.handle;
+
+import com.example.demo.exception.DianCanAuthorizeException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class DianCanExceptionHandler {
+
+
+    //拦截登录异常
+    //http://localhost:8080/diancan/leimu/list
+    @ExceptionHandler(value = DianCanAuthorizeException.class)
+    public String handlerAuthorizeException() {
+        return "zujian/loginView";
+    }
+}
