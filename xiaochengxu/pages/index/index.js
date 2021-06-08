07ec976990bcfa68f3516299074f1d95b3ad4a84
SoftWareBookService <18222333734@163.com>
2021-06-08 09:07:32 +0800

    项目页面完善重构

--- xiaochengxu/pages/index/index.js
+++ xiaochengxu/pages/index/index.js
@@ -1,48 +1,105 @@
-// index.js
-// 获取应用实例
+//index.js
 const app = getApp()
-
+let searchKey = null
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
-  },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    banner: [],//用来放轮播图的数组
   },
   onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
+    this.getTopBanner(); //请求顶部轮播图
   },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+  //页面可见
+  onShow() {
+    searchKey = '' //每次返回首页时，清空搜索词
+  },
+
+
+
+  //扫码点餐
+  btnclick1: function() {
+    let that = this;
+
+    //注意：true 扫码点餐； false 本地点餐
+    let saoMa = true;
+
+    /**
+     * 本地点餐
+     */
+    if (!saoMa) {
+      that.goToBuy("1号桌")
+      return
+    }
+
+    /**
+     * 扫码点餐
+     */
+    wx.scanCode({
       success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+        if (res.result) {
+          let str = res.result;
+          //识别的二维码里1就代表1号桌，2就代表2号桌以此类推
+          if (str.search("1") != -1) {
+            console.log("1号桌")
+            that.goToBuy("1号桌")
+          } else if (str.search("2") != -1) {
+            console.log("2号桌")
+            that.goToBuy("2号桌")
+          } else if (str.search("3") != -1) {
+            console.log("3号桌")
+            that.goToBuy("3号桌")
+          } else if (str.search("4") != -1) {
+            console.log("4号桌")
+            that.goToBuy("4号桌")
+          }
+        }
       }
     })
   },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+
+  //去点餐页
+  goToBuy(tableNum) {
+    wx.setStorageSync("tableNum", tableNum)
+    //把菜品搜索词条保存起来
+    wx.navigateTo({
+      url: '../Food/food'
+    })
+  },
+
+  //菜品浏览
+  btnclick2: function() {
+    wx.navigateTo({
+      url: '../Food/food'
+    })
+  },
+
+  btnclick4: function() {
+    console.log("跳转到后台小程序啦！")
+    wx.navigateTo({
+      url: '/pages/paihao/paihao',
+    })
+  } 
+  ,
+  //获取首页顶部轮播图
+  getTopBanner() {
+    let that = this;
+    wx.request({
+      url: app.globalData.baseUrl + '/wxPicture/getAll',
+      success: function(res) {
+        console.log("请求到的轮播图", res)
+        if (res && res.data && res.data.data && res.data.data.length > 0) {
+          let dataList = res.data.data;
+          console.log("请求到的轮播图", dataList)
+          that.setData({
+            banner: dataList
+          })
+        } else {
+          that.setData({
+            list: []
+          })
+        }
+      }
     })
   }
+
+
 })
---
+++ xiaochengxu/utils/json.js
@@ -0,0 +1,490 @@
+const json = [
+ {
+  "id": "id1",
+  "name": "name1",
+  "category": [
+   {
+    "category_id": 1,
+    "category_name": "category1",
+    "num": 0,
+   },
+   {
+    "category_id": 2,
+    "category_name": "category2",
+    "num": 0,
+   },
+   {
+    "category_id": 3,
+    "category_name": "category3",
+    "num": 0,
+   }
+  ],
+ },
+ {
+  "id": "id2",
+  "name": "name2",
+  "category": [
+   {
+    "category_id": 4,
+    "category_name": "category4",
+    "num": 0,
+   },
+   {
+    "category_id": 5,
+    "category_name": "category5",
+    "num": 0,
+   },
+   {
+    "category_id": 6,
+    "category_name": "category6",
+    "num": 0,
+   },
+   {
+    "category_id": 7,
+    "category_name": "category7",
+    "num": 0,
+   },
+   {
+    "category_id": 8,
+    "category_name": "category8",
+    "num": 0,
+   }, {
+    "category_id": 9,
+    "category_name": "category9",
+    "num": 0,
+   },
+   {
+    "category_id": 10,
+    "category_name": "category10",
+    "num": 0,
+   },
+   {
+    "category_id": 11,
+    "category_name": "category11",
+    "num": 0,
+   },
+   {
+    "category_id": 12,
+    "category_name": "category12",
+    "num": 0,
+   },
+   {
+    "category_id": 13,
+    "category_name": "category13",
+    "num": 0,
+   },
+   {
+    "category_id": 14,
+    "category_name": "category14",
+    "num": 0,
+   },
+   {
+    "category_id": 15,
+    "category_name": "category15",
+    "num": 0,
+   },
+   {
+    "category_id": 16,
+    "category_name": "category16",
+    "num": 0,
+   },
+  ]
+ },
+ {
+  "id": "id3",
+  "name": "name3",
+  "category": [
+   {
+    "category_id": 17,
+    "category_name": "category17",
+    "num": 0,
+   },
+   {
+    "category_id": 18,
+    "category_name": "category18",
+    "num": 0,
+   },
+   {
+    "category_id": 19,
+    "category_name": "category19",
+    "num": 0,
+   }
+  ],
+ },
+ {
+  "id": "id4",
+  "name": "name4",
+  "category": [
+   {
+    "category_id": 20,
+    "category_name": "category20",
+    "num": 0,
+   },
+   {
+    "category_id": 21,
+    "category_name": "category21",
+    "num": 0,
+   },
+   {
+    "category_id": 22,
+    "category_name": "category22",
+    "num": 0,
+   },
+   {
+    "category_id": 23,
+    "category_name": "category23",
+    "num": 0,
+   },
+   {
+    "category_id": 24,
+    "category_name": "category24",
+    "num": 0,
+   },
+   {
+    "category_id": 25,
+    "category_name": "category25",
+    "num": 0,
+   },
+   {
+    "category_id": 26,
+    "category_name": "category26",
+    "num": 0,
+   },
+   {
+    "category_id": 27,
+    "category_name": "category27",
+    "num": 0,
+   },
+   {
+    "category_id": 28,
+    "category_name": "category28",
+    "num": 0,
+   },
+   {
+    "category_id": 29,
+    "category_name": "category29",
+    "num": 0,
+   },
+   {
+    "category_id": 30,
+    "category_name": "category30",
+    "num": 0,
+   },
+   {
+    "category_id": 31,
+    "category_name": "category31",
+    "num": 0,
+   },
+   {
+    "category_id": 32,
+    "category_name": "category32",
+    "num": 0,
+   }
+  ],
+ },
+ {
+  "id": "id5",
+  "name": "name5",
+  "category": [
+   {
+    "category_id": 33,
+    "category_name": "category33",
+    "num": 0,
+   },
+   {
+    "category_id": 34,
+    "category_name": "category34",
+    "num": 0,
+   },
+   {
+    "category_id": 35,
+    "category_name": "category35",
+    "num": 0,
+   }
+  ],
+ },
+ {
+  "id": "id6",
+  "name": "name6",
+  "category": [
+   {
+    "category_id": 36,
+    "category_name": "category36",
+    "num": 0,
+   },
+   {
+    "category_id": 37,
+    "category_name": "category37",
+    "num": 0,
+   },
+   {
+    "category_id": 38,
+    "category_name": "category38",
+    "num": 0,
+   }
+  ],
+ },
+ {
+  "id": "id7",
+  "name": "name7",
+  "category": [
+   {
+    "category_id": 39,
+    "category_name": "category39",
+    "num": 0,
+   },
+   {
+    "category_id": 40,
+    "category_name": "category40",
+    "num": 0,
+   },
+   {
+    "category_id": 41,
+    "category_name": "category41",
+    "num": 0,
+   }
+  ],
+ },
+ {
+  "id": "id8",
+  "name": "name8",
+  "category": [
+   {
+    "category_id": 42,
+    "category_name": "category42",
+    "num": 0,
+   },
+   {
+    "category_id": 43,
+    "category_name": "category43",
+    "num": 0,
+   },
+   {
+    "category_id": 44,
+    "category_name": "category44",
+    "num": 0,
+   }
+  ],
+ },
+ {
+  "id": "id9",
+  "name": "name9",
+  "category": [
+   {
+    "category_id": 45,
+    "category_name": "category45",
+    "num": 0,
+   },
+   {
+    "category_id": 46,
+    "category_name": "category46",
+    "num": 0,
+   },
+   {
+    "category_id": 47,
+    "category_name": "category47",
+    "num": 0,
+   }
+  ],
+ },
+ {
+  "id": "id10",
+  "name": "name10",
+  "category": [
+   {
+    "category_id": 48,
+    "category_name": "category48",
+    "num": 0,
+   },
+   {
+    "category_id": 49,
+    "category_name": "category49",
+    "num": 0,
+   },
+   {
+    "category_id": 50,
+    "category_name": "category50",
+    "num": 0,
+   }
+  ],
+ },
+ {
+  "id": "id11",
+  "name": "name11",
+  "category": [
+   {
+    "category_id": 51,
+    "category_name": "category51",
+    "num": 0,
+   },
+   {
+    "category_id": 52,
+    "category_name": "category52",
+    "num": 0,
+   },
+   {
+    "category_id": 53,
+    "category_name": "category53",
+    "num": 0,
+   }
+  ],
+ },
+ {
+  "id": "id12",
+  "name": "name12",
+  "category": [
+   {
+    "category_id": 54,
+    "category_name": "category54",
+    "num": 0,
+   },
+   {
+    "category_id": 55,
+    "category_name": "category55",
+    "num": 0,
+   },
+   {
+    "category_id": 56,
+    "category_name": "category56",
+    "num": 0,
+   }
+  ],
+ },
+ {
+  "id": "id13",
+  "name": "name13",
+  "category": [
+   {
+    "category_id": 57,
+    "category_name": "category57",
+    "num": 0,
+   },
+   {
+    "category_id": 58,
+    "category_name": "category58",
+    "num": 0,
+   },
+   {
+    "category_id": 59,
+    "category_name": "category59",
+    "num": 0,
+   }
+  ],
+ },
+ {
+  "id": "id14",
+  "name": "name14",
+  "category": [
+   {
+    "category_id": 60,
+    "category_name": "category60",
+    "num": 0,
+   },
+   {
+    "category_id": 61,
+    "category_name": "category61",
+    "num": 0,
+   },
+   {
+    "category_id": 62,
+    "category_name": "category62",
+    "num": 0,
+   }
+  ],
+ },
+ {
+  "id": "id15",
+  "name": "name15",
+  "category": [
+   {
+    "category_id": 63,
+    "category_name": "category63",
+    "num": 0,
+   },
+   {
+    "category_id": 64,
+    "category_name": "category64",
+    "num": 0,
+   },
+   {
+    "category_id": 65,
+    "category_name": "category65",
+    "num": 0,
+   }
+  ],
+ },
+ {
+  "id": "id16",
+  "name": "name16",
+  "category": [
+   {
+    "category_id": 66,
+    "category_name": "category66",
+    "num": 0,
+   },
+   {
+    "category_id": 67,
+    "category_name": "category67",
+    "num": 0,
+   },
+   {
+    "category_id": 68,
+    "category_name": "category68",
+    "num": 0,
+   },
+   {
+    "category_id": 69,
+    "category_name": "category69",
+    "num": 0,
+   },
+   {
+    "category_id": 70,
+    "category_name": "category70",
+    "num": 0,
+   },
+   {
+    "category_id": 71,
+    "category_name": "category71",
+    "num": 0,
+   },
+   {
+    "category_id": 72,
+    "category_name": "category72",
+    "num": 0,
+   },
+   {
+    "category_id": 73,
+    "category_name": "category73",
+    "num": 0,
+   },
+   {
+    "category_id": 74,
+    "category_name": "category74",
+    "num": 0,
+   },
+   {
+    "category_id": 75,
+    "category_name": "category75",
+    "num": 0,
+   },
+   {
+    "category_id": 76,
+    "category_name": "category76",
+    "num": 0,
+   },
+   {
+    "category_id": 77,
+    "category_name": "category77",
+    "num": 0,
+   },
+   {
+    "category_id": 78,
+    "category_name": "category78",
+    "num": 0,
+   }
+  ],
+ },
+]
+
+module.exports = json
+
